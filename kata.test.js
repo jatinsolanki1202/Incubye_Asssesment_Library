273c970b2b38e2798ec960cc6f8a31e7b0adac74
jatin solanki <jatinsolanki1202@gmail.com>
2024-08-10 23:31:37 +0530

    Added test case for properly adding books if all the details are filled properly

--- kata.test.js
+++ kata.test.js
@@ -13,4 +13,9 @@ describe('tests for adding books', () => {
     test('should show error if any field left empty', () => {
         expect(kl.addBook(101, "", "Chris Gardner", 2006)).toBe("Please enter valid book details");
     })
+
+    test('should properly add books if all details are filled correctly', () => {
+        expect(kl.addBook(102, "The Guide", "RK Narayan", 1958)).toBe("Book added: The Guide")
+        expect(kl.addBook(103, "The People of the Indus", "Nikhl Gulati", 2022)).toBe("Book added: The People of the Indus")
+    })
 })
