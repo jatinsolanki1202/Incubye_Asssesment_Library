b33c6f8a37e2ccfb688b0eacd577d963bce0862c
jatin solanki <jatinsolanki1202@gmail.com>
2024-08-10 23:09:21 +0530

    written third test, if any of the field left empty, error should be shown

--- kata.test.js
+++ kata.test.js
@@ -9,4 +9,8 @@ describe('tests for adding books', () => {
     test('should show error message if book with same isbn received', () => {
         expect(kl.addBook(101, "The Guide", "RK Narayan", 1958)).toBe("Cannot add book with same isbn number")
     })
+
+    test('should show error if any field left empty', () => {
+        expect(kl.addBook(101, "", "Chris Gardner", 2006)).toBe("Please enter valid book details");
+    })
 })
