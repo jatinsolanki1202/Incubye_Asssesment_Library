21ef1754f51a554921273013f7f7ee41a2b79934
jatin solanki <jatinsolanki1202@gmail.com>
2024-08-10 21:52:31 +0530

    second test case for showing error for same isbn number

--- kata.test.js
+++ kata.test.js
@@ -3,6 +3,10 @@ import { KataLibrary } from "./index";
 const kl = new KataLibrary();
 describe('tests for adding books', () => {
     test('should add a book', () => {
-        expect(kl.addBook(101, "hello", "world", 2002)).toBe("Book added: hello");
+        expect(kl.addBook(101, "Pursuit of Happyness", "Chris Gardner", 2006)).toBe("Book added: hello");
+    })
+
+    test('should show error message if book with same isbn received', () => {
+        expect(kl.addBook(101, "The Guide", "RK Narayan", 1958)).toBe("Cannot add book with same isbn number")
     })
 })
