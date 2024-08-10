4e692a94454e9daabb16d3e9d4ac9a4ff33b57ae
jatin solanki <jatinsolanki1202@gmail.com>
2024-08-10 23:38:58 +0530

    Add Borrow test - book should be borrowed if it is available

--- kata.test.js
+++ kata.test.js
@@ -1,6 +1,8 @@
 import { KataLibrary } from "./index";
 
 const kl = new KataLibrary();
+
+// Tests for adding books
 describe('tests for adding books', () => {
     test('should add a book', () => {
         expect(kl.addBook(101, "Pursuit of Happyness", "Chris Gardner", 2006)).toBe("Book added: Pursuit of Happyness");
@@ -17,5 +19,12 @@ describe('tests for adding books', () => {
     test('should properly add books if all details are filled correctly', () => {
         expect(kl.addBook(102, "The Guide", "RK Narayan", 1958)).toBe("Book added: The Guide")
         expect(kl.addBook(103, "The People of the Indus", "Nikhl Gulati", 2022)).toBe("Book added: The People of the Indus")
+    })
+})
+
+// Tests for borrowing books
+describe('tests for borrowing books', () => {
+    test('should borrow a book if it is available', () => {
+        expect(kl.borrowBook(101)).toBe("Book borrowed: Pursuit of Happyness")
     })
 })
